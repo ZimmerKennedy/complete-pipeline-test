662055f2098bb0bd09d70a4b44cef45fc34d5463
Zimmer Kennedy <kennedyzimmer00@gmail.com>
2023-10-18 13:27:58 +0300

    first commit

---
+++ src/main/java/com/completepipeline/completepipeline/CompletePipelineApplication.java
@@ -0,0 +1,13 @@
+package com.completepipeline.completepipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompletePipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompletePipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/completepipeline/completepipeline/CompletePipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.completepipeline.completepipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompletePipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
